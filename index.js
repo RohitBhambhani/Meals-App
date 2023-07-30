712f1c6be23aa92569ff5f1981c3a20baea61d64
RohitBhambhani <rohitbhambhani9@gmail.com>
2023-07-30 18:49:10 +0530

    Open Drawer functionality

---
+++ index.js
@@ -0,0 +1,7 @@
+function openDrawer() {
+	const drawer = document.getElementById("drawer")
+	drawer.classList.add("transition-opacity");
+	drawer.classList.add("opacity-100");
+	drawer.classList.add("duration-500");
+	drawer.classList.add("translate-x-0");
+}
